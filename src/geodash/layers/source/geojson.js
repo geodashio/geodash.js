612eaea802d9d7d468849e918b19a28c206c9fca
pjdufour <pjdufour.dev@gmail.com>
2016-11-03 13:46:17 +0100

    projections for local geojson

--- src/geodash/layers/source/geojson.js
+++ src/geodash/layers/source/geojson.js
@@ -2,15 +2,19 @@ module.exports = function(options)
 {
   var source = undefined;
 
-  var projection = "EPSG:4326";
   var local = extract("local", options);
   var url = extract("url", options);
+  var projection = extract("projection", options, "EPSG:4326");
 
   if(angular.isDefined(local))
   {
+    // For discussion on handling projections, see
+    // http://stackoverflow.com/questions/32455040/how-to-specify-the-projection-for-geojson-in-openlayers3
     source = new ol.source.Vector({
-      features: (new ol.format.GeoJSON()).readFeatures(local),
-      projection: projection,
+      features: (new ol.format.GeoJSON()).readFeatures(local, {
+        dataProjection: projection,
+        featureProjection: "EPSG:3857"
+      })
     });
   }
   else if(angular.isDefined(url))
