8745b0d12d0d93bff716da56c8e139087fa6d2df
pjdufour <pjdufour.dev@gmail.com>
2017-04-19 00:45:44 -0400

    charts

--- src/geodash/api/getRelatedEdges.js
+++ src/geodash/api/getRelatedEdges.js
@@ -4,7 +4,7 @@ module.exports = function(graph_id, entity_id)
 {
   var edges = getEdges(graph_id);
   var matches = edges.filter(function(edge){
-    return edge.source.id == entity_id && edge.target.id == entity_id;
+    return edge.source.id == entity_id || edge.target.id == entity_id;
   });
   return matches;
 };
